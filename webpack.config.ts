5669e915a57bd2772fe4fc30e57ab9c47a37b8e6
pakerchang <pakerchang@gmail.com>
2023-08-06 22:50:56 +0800

    feat(tools): Add html template parser plugin.

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,7 @@
 import * as path from "path";
 import * as webpack from "webpack";
 import "webpack-dev-server";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 const config: webpack.Configuration = {
   mode: "development",
@@ -20,8 +21,15 @@ const config: webpack.Configuration = {
   resolve: {
     extensions: [".ts", ".js", ".tsx", ".d.ts"],
     modules: [path.resolve(__dirname, "node_modules"), path.resolve(__dirname, "./packages")],
+    fallback: {
+      events: require.resolve("events/"),
+    },
   },
-  plugins: [],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./dist/index.html",
+    }),
+  ],
 };
 
 export default config;
