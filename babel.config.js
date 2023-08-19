164f65634ebad9f972ea7eb429774198f8e17649
pakerchang <pakerchang@gmail.com>
2023-08-19 16:21:04 +0800

    ref: reinstall react to builder peerb depenscies

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,14 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-react",
+    [
+      "@babel/preset-typescript",
+      {
+        isTSX: true,
+        allExtensions: true,
+      },
+    ],
+  ],
   plugins: [["@babel/plugin-transform-runtime", {}]],
 };
--- index.ts
+++
@@ -1,3 +0,0 @@
-const temp='testing'
-
-console.log(temp)
